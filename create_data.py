bbc5f065e974708b0fa2899ab7ed4b914c209dad
eric2i <eric2ihsiung@gmail.com>
2024-03-08 22:05:17 -0800

    Optimization for building potential large dataset: - Replace row-wise data entry conversion with pandas nested .apply() method; - Reduced system I/O operations by executing f.write() only once with a complete jsonified_dataset.

--- create_data.py
+++ create_data.py
@@ -1,28 +1,41 @@
 import json
-import pandas as pd
 from datasets import load_dataset
+import argparse
+from tqdm import trange
 
 DEFAULT_SYSTEM_PROMPT = "You are a teaching assistant for Ubuntu System. You should help the user to answer his question."
 
 
-def create_dataset(question, answer):
-    return {
+def create_entry(question, answer):
+    return json.dumps({
         "messages": [
             {"role": "system", "content": DEFAULT_SYSTEM_PROMPT},
             {"role": "user", "content": question},
             {"role": "assistant", "content": answer},
         ]
-    }
+    })
+
+
+def generate(sample_size):
+    # Load the dataset
+    df = load_dataset("mugithi/ubuntu_question_answer")
+    print(df)
+
+    # Sample the dataset
+    train_data = df["train"].to_pandas().sample(sample_size, random_state=42)
+    train_data.reset_index(drop=True, inplace=True)
+    print("The shape of train_data is:", train_data.shape)
+
+    # Create the training dataset
+    jsonified_dataset = train_data.apply(lambda x: create_entry(x["question"], x["answer"]), axis=1).str.cat(sep="\n")
+    with open("train_data.jsonl", "w") as f:
+        f.write(jsonified_dataset)
+    print("Successfully Create Training Dataset!")
 
 
 if __name__ == "__main__":
-    df = load_dataset("mugithi/ubuntu_question_answer")
-    print(df)
-    train_data = df["train"].to_pandas().sample(100)
-    train_data.reset_index(drop=True, inplace=True)
-    print("The shape of train_data is:", train_data.shape)
-    with open("train_data.jsonl", "w") as f:
-        for idx, row in train_data.iterrows():
-            data = json.dumps(create_dataset(row["question"], row["answer"]))
-            f.write(data + "\n")
-    print("Successfully Create Training Dataset!")
+    parser = argparse.ArgumentParser(description="Create Training Dataset")
+    parser.add_argument("-s", "--sample_size", type=int, default=100, help="The size of the training dataset")
+    args = parser.parse_args()
+
+    generate(args.sample_size)
